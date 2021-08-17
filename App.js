c18c5d98cb487c43eb69159ec880b35728edc37a
JegaMax <70192900+JegaMax@users.noreply.github.com>
2021-08-17 19:01:31 +0530

    ReactJs program Day 1 assignment

---
+++ App.js
@@ -0,0 +1,14 @@
+import CareerDay from "./CareerDay";
+import { MachineLearning, ReactJsprgrm } from "./LetsUpgrade";
+
+function App() {
+  return (
+    <div className="App">
+      <ReactJsprgrm />
+      <MachineLearning />
+      <CareerDay />
+    </div>
+  );
+}
+
+export default App;
